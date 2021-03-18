cbd6a266e2f9770b3eef29756a2c4ccc88c3d429
kamel <kamelyakhlef@hotmail.fr>
2021-03-18 20:02:15 +0100

    create decks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SheetsModule } from './sheets/sheets.module';
+import { DecksModule } from './decks/decks.module';
 
 @Module({
   imports: [
     SheetsModule,
     MongooseModule.forRoot('mongodb://localhost:27017/revision'),
+    DecksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
