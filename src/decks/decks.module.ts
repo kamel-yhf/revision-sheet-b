b4a2433ec028869e5dd659d50b7952f59cfed9c2
kamel <kamelyakhlef@hotmail.fr>
2021-03-18 20:04:11 +0100

    create decks service

--- src/decks/decks.module.ts
+++ src/decks/decks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DecksController } from './decks.controller';
+import { DecksService } from './decks.service';
 
 @Module({
-  controllers: [DecksController]
+  controllers: [DecksController],
+  providers: [DecksService]
 })
 export class DecksModule {}
