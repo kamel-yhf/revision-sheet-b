71c0f911f030a3c5c01f505af07095147ddcb359
kamel <kamelyakhlef@hotmail.fr>
2021-03-22 14:30:32 +0100

    hash user password ok

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,27 +3,40 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Deck } from 'src/decks/deck.model';
 import { User } from './user.model';
+const bcrypt = require('bcrypt');
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   //create user
-  async insertUser(username: string, email: string, password: string, deck: Deck){
-      const newUser = new this.userModel({
-          username: username,
-          email: email,
-          password: password,
-          deck: deck,
-      });
-      const result = await newUser.save();
-      return result.id as string;
+  async insertUser(
+    username: string,
+    email: string,
+    password: string,
+    deck: Deck,
+  ) {
+    const hash = this.hashPassword(password);
+    const newUser = new this.userModel({
+      username: username,
+      email: email,
+      password: hash,
+      deck: deck,
+    });
+    const result = await newUser.save();
+    return result.id as string;
   }
 
   //get all users
-  async getAllUsers(){
+  async getAllUsers() {
     const users = await this.userModel.find().exec();
     return users;
   }
-}
 
+  hashPassword(password) {
+    const salt = 10;
+    const hash = bcrypt.hashSync(password, salt);
+
+    return hash;
+  }
+}
